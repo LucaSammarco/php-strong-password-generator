b51cd669ae6f3849281f6a5ef5476c1e563e698f
Luca Sammarco <luca.sammarco7@gmail.com>
2024-06-28 18:32:41 +0200

    set up

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+ require_once __DIR__ . '/utilities/functions.php';
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ utilities/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function isEven($number) {
+    return $number % 2 === 0;
+};
+
+
+function gerRandomInt($min, $max) {
+    return rand($min, $max);
+};
+
+
+?>
